a03890dcee96ef6833322582d686649beb110973
BrooklynT101 <brooklynigtaylor@gmail.com>
2025-10-05 23:54:34 +1300

    Shifted dockerfiles in vm subfolders:
    - secrets are now centralised by script needing them
    - crashes when run, possibly server.js

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const { Pool } = require('pg');
 
 import dotenv from 'dotenv';
-dotenv.config({ path: '../.env' });     // shared
+dotenv.config({ path: '../env/local.env' });     // shared
 
 const app = express();
 app.use(express.json());
